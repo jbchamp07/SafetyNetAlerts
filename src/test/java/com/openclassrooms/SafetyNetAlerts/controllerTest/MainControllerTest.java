a63722adce14db0ec2c986a9df4105041964c04b
Champetier <jb.champetier@gmail.com>
2022-03-02 15:35:24 +0100

    start controller test

--- src/test/java/com/openclassrooms/SafetyNetAlerts/controllerTest/MainControllerTest.java
+++ src/test/java/com/openclassrooms/SafetyNetAlerts/controllerTest/MainControllerTest.java
@@ -2,6 +2,7 @@ package com.openclassrooms.SafetyNetAlerts.controllerTest;
 
 import com.openclassrooms.SafetyNetAlerts.controller.MainController;
 import com.openclassrooms.SafetyNetAlerts.service.PersonServices;
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -9,23 +10,35 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.RequestBuilder;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+
 @WebMvcTest(controllers = MainController.class)
-@AutoConfigureMockMvc
+//@SpringBootTest
+//@AutoConfigureMockMvc
 public class MainControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
-
     @MockBean
     private PersonServices personServices;
 
+    @Before
+    public void init(){
+    }
+
     @Test
-    public void listPersonOfAFireStationTest(){
-        mockMvc.perform("/firestation").andExpect(status()).isOk();
+    public void shouldCreateMockMvc(){
+        assertNotNull(mockMvc);
+    }
+
+    @Test
+    public void listPersonOfAFireStationTest() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/firestation").param("stationNumber", String.valueOf(1))).andExpect(MockMvcResultMatchers.status().isOk());
     }
 
 }
