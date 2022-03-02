5931921782d80d9437c950ad848753635f8b830d
Champetier <jb.champetier@gmail.com>
2022-03-02 16:53:56 +0100

    start controller test

--- src/test/java/com/openclassrooms/SafetyNetAlerts/serviceTest/PersonServicesTest.java
+++ src/test/java/com/openclassrooms/SafetyNetAlerts/serviceTest/PersonServicesTest.java
@@ -4,12 +4,13 @@ import com.openclassrooms.SafetyNetAlerts.model.Person;
 import com.openclassrooms.SafetyNetAlerts.service.IDataServices;
 import com.openclassrooms.SafetyNetAlerts.service.PersonServices;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.HashMap;
 import java.util.List;
@@ -18,10 +19,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest
-@AutoConfigureMockMvc
+@ContextConfiguration
 public class PersonServicesTest {
-    @Autowired
-    private MockMvc mockMvc;
+
     @Autowired
     private PersonServices personServices;
 
